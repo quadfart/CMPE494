c333d215e7ea04246a011982d0c6fcc083ef3846
quadfart <quadfart@gmail.com>
2024-12-25 08:25:41 +0300

    christmas hotfix2

--- PlanAppAPI/PlanAppAPI/Applications/SensorData/DiseasePrediction/DiseasePrediction.cs
+++ PlanAppAPI/PlanAppAPI/Applications/SensorData/DiseasePrediction/DiseasePrediction.cs
@@ -34,7 +34,7 @@ public class Handler : IRequestHandler<Command, Result<List<DiseasePredictionVie
         try
         {
             // API endpoint
-            var apiUrl = "http://ec2-13-53-214-163.eu-north-1.compute.amazonaws.com/predictDisease";
+            var apiUrl = "http://ec2-13-53-214-163.eu-north-1.compute.amazonaws.com:5000/predictDisease";
 
             using var httpClient = new HttpClient();
             using var form = new MultipartFormDataContent();
