694806ddf51a2dfa8fe141925e72449f74704214
quadfart <quadfart@gmail.com>
2024-12-27 04:08:01 +0300

    image resize on dotnet api

--- PlanAppAPI/PlanAppAPI/Applications/Plant/Prediction.cs
+++ PlanAppAPI/PlanAppAPI/Applications/Plant/Prediction.cs
@@ -1,3 +1,4 @@
+using System.Drawing; // Add reference to System.Drawing.Common
 using System.Text;
 using MediatR;
 using Newtonsoft.Json;
@@ -13,88 +14,101 @@ namespace PlanAppAPI.Applications.Plant;
 public class Prediction
 {
     public class Command : IRequest<Result<List<PlantPredictionViewModel>>>
-{
-    public required IFormFile File { get; set; }
-}
-
-public class Handler : IRequestHandler<Command, Result<List<PlantPredictionViewModel>>>
-{
-    private readonly ILogger<Prediction> _logger;
-    private readonly DataContext _context;
-
-    public Handler(ILogger<Prediction> logger, DataContext context)
     {
-        _logger = logger;
-        _context = context;
+        public required IFormFile File { get; set; }
     }
 
-    public async Task<Result<List<PlantPredictionViewModel>>> Handle(Command request, CancellationToken cancellationToken)
+    public class Handler : IRequestHandler<Command, Result<List<PlantPredictionViewModel>>>
     {
-        try
+        private readonly ILogger<Prediction> _logger;
+        private readonly DataContext _context;
+
+        public Handler(ILogger<Prediction> logger, DataContext context)
         {
-            // API endpoint
-            var apiUrl = "http://cmpe494-flask-api-1:5000/predict";
+            _logger = logger;
+            _context = context;
+        }
 
-            using var httpClient = new HttpClient();
-            using var form = new MultipartFormDataContent();
-
-            // Read the file content from the request
-            using var fileStream = request.File.OpenReadStream();
-            var fileContent = new StreamContent(fileStream);
-
-            // Set content headers
-            fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(request.File.ContentType);
-            form.Add(fileContent, "file", request.File.FileName);
-
-            // Send the request to the API
-            var response = await httpClient.PostAsync(apiUrl, form);
-
-            if (response.IsSuccessStatusCode)
+        public async Task<Result<List<PlantPredictionViewModel>>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            try
             {
-                // Parse the API response
-                var content = await response.Content.ReadAsStringAsync();
-                _logger.LogInformation("Prediction Response: {Content}", content);
-                var predictionResponseModel = JsonConvert.DeserializeObject<PredictionResponseModel>(content);
+                // API endpoint
+                var apiUrl = "http://cmpe494-flask-api-1:5000/predict";
 
-                var predictionResponse = predictionResponseModel.PredictionResponse
-                    .ToDictionary(x => x.Key, x => float.Parse(x.Value));
-                
-                var orderedPredictionResponse = predictionResponse
-                    .OrderByDescending(x => x.Value)
-                    .Take(5)
-                    .ToDictionary(x => x.Key, x => x.Value);
+                using var httpClient = new HttpClient();
+                using var form = new MultipartFormDataContent();
 
-                var matchingPlants = await _context.Plants
-                    .Where(plant => orderedPredictionResponse.Keys.Contains(plant.ScientificName))
-                    .ToListAsync(cancellationToken);
-                var responseModel = matchingPlants.Select(x => new PlantPredictionViewModel
+                // Resize the image to 224x224
+                var resizedImage = ResizeImage(request.File, 224, 224);
+
+                // Convert the resized image to a stream
+                var resizedStream = new MemoryStream(resizedImage);
+                var fileContent = new StreamContent(resizedStream);
+
+                // Set content headers
+                fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(request.File.ContentType);
+                form.Add(fileContent, "file", request.File.FileName);
+
+                // Send the request to the API
+                var response = await httpClient.PostAsync(apiUrl, form);
+
+                if (response.IsSuccessStatusCode)
                 {
-                    Id = x.Id,
-                    ModTemp = x.ModTemp,
-                    SoilType = x.SoilType,
-                    LightNeed = x.LightNeed,
-                    HumidityLevel = x.HumidityLevel,
-                    WateringFrequency = x.WateringFrequency,
-                    IrrigationAmount = x.IrrigationAmount,
-                    ScientificName = x.ScientificName,
-                    PredictionConfidence = predictionResponse.GetValueOrDefault(x.ScientificName)
-                }).OrderByDescending(x=>x.PredictionConfidence).ToList();
+                    // Parse the API response
+                    var content = await response.Content.ReadAsStringAsync();
+                    _logger.LogInformation("Prediction Response: {Content}", content);
+                    var predictionResponseModel = JsonConvert.DeserializeObject<PredictionResponseModel>(content);
 
-                return Result<List<PlantPredictionViewModel>>.Success(responseModel);
+                    var predictionResponse = predictionResponseModel.PredictionResponse
+                        .ToDictionary(x => x.Key, x => float.Parse(x.Value));
+
+                    var orderedPredictionResponse = predictionResponse
+                        .OrderByDescending(x => x.Value)
+                        .Take(5)
+                        .ToDictionary(x => x.Key, x => x.Value);
+
+                    var matchingPlants = await _context.Plants
+                        .Where(plant => orderedPredictionResponse.Keys.Contains(plant.ScientificName))
+                        .ToListAsync(cancellationToken);
+
+                    var responseModel = matchingPlants.Select(x => new PlantPredictionViewModel
+                    {
+                        Id = x.Id,
+                        ModTemp = x.ModTemp,
+                        SoilType = x.SoilType,
+                        LightNeed = x.LightNeed,
+                        HumidityLevel = x.HumidityLevel,
+                        WateringFrequency = x.WateringFrequency,
+                        IrrigationAmount = x.IrrigationAmount,
+                        ScientificName = x.ScientificName,
+                        PredictionConfidence = predictionResponse.GetValueOrDefault(x.ScientificName)
+                    }).OrderByDescending(x => x.PredictionConfidence).ToList();
+
+                    return Result<List<PlantPredictionViewModel>>.Success(responseModel);
+                }
+                else
+                {
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    _logger.LogError("Prediction API Error: {Error}", errorContent);
+                    throw new Exception($"API Error: {errorContent}");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                var errorContent = await response.Content.ReadAsStringAsync();
-                _logger.LogError("Prediction API Error: {Error}", errorContent);
-                throw new Exception($"API Error: {errorContent}");
+                _logger.LogError("Error in Plant Prediction: {Message}", ex.Message);
+                throw new Exception("Error in Plant Prediction: " + ex.Message, ex);
             }
         }
-        catch (Exception ex)
+
+        private byte[] ResizeImage(IFormFile file, int width, int height)
         {
-            _logger.LogError("Error in Plant Prediction: {Message}", ex.Message);
-            throw new Exception("Error in Plant Prediction: " + ex.Message, ex);
+            using var inputStream = file.OpenReadStream();
+            using var image = Image.FromStream(inputStream);
+            using var resized = new Bitmap(image, new Size(width, height));
+            using var outputStream = new MemoryStream();
+            resized.Save(outputStream, image.RawFormat);
+            return outputStream.ToArray();
         }
     }
-}
-
 }
--- PlanAppAPI/PlanAppAPI/Applications/SensorData/DiseasePrediction/DiseasePrediction.cs
+++ PlanAppAPI/PlanAppAPI/Applications/SensorData/DiseasePrediction/DiseasePrediction.cs
@@ -1,3 +1,4 @@
+using System.Drawing; // Add reference to System.Drawing.Common
 using System.Text;
 using MediatR;
 using Newtonsoft.Json;
@@ -14,84 +15,97 @@ namespace PlanAppAPI.Applications.SensorData.DiseasePrediction;
 public class DiseasePrediction
 {
     public class Command : IRequest<Result<List<DiseasePredictionViewModel>>>
-{
-    public required IFormFile File { get; set; }
-}
-
-public class Handler : IRequestHandler<Command, Result<List<DiseasePredictionViewModel>>>
-{
-    private readonly ILogger<DiseasePrediction> _logger;
-    private readonly DataContext _context;
-
-    public Handler(ILogger<DiseasePrediction> logger, DataContext context)
     {
-        _logger = logger;
-        _context = context;
+        public required IFormFile File { get; set; }
     }
 
-    public async Task<Result<List<DiseasePredictionViewModel>>> Handle(Command request, CancellationToken cancellationToken)
+    public class Handler : IRequestHandler<Command, Result<List<DiseasePredictionViewModel>>>
     {
-        try
+        private readonly ILogger<DiseasePrediction> _logger;
+        private readonly DataContext _context;
+
+        public Handler(ILogger<DiseasePrediction> logger, DataContext context)
         {
-            // API endpoint
-            var apiUrl = "http://cmpe494-flask-api-1:5000/predictDisease";
+            _logger = logger;
+            _context = context;
+        }
 
-            using var httpClient = new HttpClient();
-            using var form = new MultipartFormDataContent();
-
-            // Read the file content from the request
-            using var fileStream = request.File.OpenReadStream();
-            var fileContent = new StreamContent(fileStream);
-
-            // Set content headers
-            fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(request.File.ContentType);
-            form.Add(fileContent, "file", request.File.FileName);
-
-            // Send the request to the API
-            var response = await httpClient.PostAsync(apiUrl, form);
-
-            if (response.IsSuccessStatusCode)
+        public async Task<Result<List<DiseasePredictionViewModel>>> Handle(Command request, CancellationToken cancellationToken)
+        {
+            try
             {
-                // Parse the API response
-                var content = await response.Content.ReadAsStringAsync();
-                _logger.LogInformation("Prediction Response: {Content}", content);
-                var predictionResponseModel = JsonConvert.DeserializeObject<DiseasePredictionResponseModel>(content);
+                // API endpoint
+                var apiUrl = "http://cmpe494-flask-api-1:5000/predictDisease";
 
-                var predictionResponse = predictionResponseModel.DiseasePredictionResponse
-                    .ToDictionary(x => x.Key, x => float.Parse(x.Value));
-                
-                var orderedPredictionResponse = predictionResponse
-                    .OrderByDescending(x => x.Value)
-                    .Take(3)
-                    .ToDictionary(x => x.Key, x => x.Value);
+                using var httpClient = new HttpClient();
+                using var form = new MultipartFormDataContent();
 
-                var matchingDiseases = await _context.Diseases
-                    .Where(disease => orderedPredictionResponse.Keys.Contains(disease.Name))
-                    .ToListAsync(cancellationToken);
-                var responseModel = matchingDiseases.Select(x => new DiseasePredictionViewModel
+                // Resize the image to 224x224
+                var resizedImage = ResizeImage(request.File, 224, 224);
+
+                // Convert the resized image to a stream
+                var resizedStream = new MemoryStream(resizedImage);
+                var fileContent = new StreamContent(resizedStream);
+
+                // Set content headers
+                fileContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(request.File.ContentType);
+                form.Add(fileContent, "file", request.File.FileName);
+
+                // Send the request to the API
+                var response = await httpClient.PostAsync(apiUrl, form);
+
+                if (response.IsSuccessStatusCode)
                 {
-                    Id = x.Id,
-                    Name = x.Name,
-                    Symptoms = x.Symptoms,
-                    Treatments = x.Treatments,
-                    DiseasePredictionConfidence = predictionResponse.GetValueOrDefault(x.Name)
-                }).OrderByDescending(x=>x.DiseasePredictionConfidence).ToList();
+                    // Parse the API response
+                    var content = await response.Content.ReadAsStringAsync();
+                    _logger.LogInformation("Prediction Response: {Content}", content);
+                    var predictionResponseModel = JsonConvert.DeserializeObject<DiseasePredictionResponseModel>(content);
 
-                return Result<List<DiseasePredictionViewModel>>.Success(responseModel);
+                    var predictionResponse = predictionResponseModel.DiseasePredictionResponse
+                        .ToDictionary(x => x.Key, x => float.Parse(x.Value));
+
+                    var orderedPredictionResponse = predictionResponse
+                        .OrderByDescending(x => x.Value)
+                        .Take(3)
+                        .ToDictionary(x => x.Key, x => x.Value);
+
+                    var matchingDiseases = await _context.Diseases
+                        .Where(disease => orderedPredictionResponse.Keys.Contains(disease.Name))
+                        .ToListAsync(cancellationToken);
+
+                    var responseModel = matchingDiseases.Select(x => new DiseasePredictionViewModel
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Symptoms = x.Symptoms,
+                        Treatments = x.Treatments,
+                        DiseasePredictionConfidence = predictionResponse.GetValueOrDefault(x.Name)
+                    }).OrderByDescending(x => x.DiseasePredictionConfidence).ToList();
+
+                    return Result<List<DiseasePredictionViewModel>>.Success(responseModel);
+                }
+                else
+                {
+                    var errorContent = await response.Content.ReadAsStringAsync();
+                    _logger.LogError("Prediction API Error: {Error}", errorContent);
+                    throw new Exception($"API Error: {errorContent}");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                var errorContent = await response.Content.ReadAsStringAsync();
-                _logger.LogError("Prediction API Error: {Error}", errorContent);
-                throw new Exception($"API Error: {errorContent}");
+                _logger.LogError("Error in Disease Prediction: {Message}", ex.Message);
+                throw new Exception("Error in Disease Prediction: " + ex.Message, ex);
             }
         }
-        catch (Exception ex)
+
+        private byte[] ResizeImage(IFormFile file, int width, int height)
         {
-            _logger.LogError("Error in Plant Prediction: {Message}", ex.Message);
-            throw new Exception("Error in Plant Prediction: " + ex.Message, ex);
+            using var inputStream = file.OpenReadStream();
+            using var image = Image.FromStream(inputStream);
+            using var resized = new Bitmap(image, new Size(width, height));
+            using var outputStream = new MemoryStream();
+            resized.Save(outputStream, image.RawFormat);
+            return outputStream.ToArray();
         }
     }
-}
-
 }
